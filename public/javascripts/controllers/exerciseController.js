b652b61447cf0da9fe405473725ef3ab447e9f6d
Kelvin Yu <kelvin.yu98@gmail.com>
2017-02-08 01:18:17 -0500

    styling and begin adding calendar

--- public/javascripts/controllers/exerciseController.js
+++ public/javascripts/controllers/exerciseController.js
@@ -1,11 +1,32 @@
 var app = angular.module('workoutApp');
 
 app.controller('exerciseController', function($scope, $location, exerciseService){
-	$scope.newExercise = {name:''};
-	$scope.exercises = exerciseService.query();	
-	$scope.post = function(){
+	//view exercises
+	$scope.newExercise = {name:'', muscles_worked:[], force:[], equipment:[]};
+	$scope.exercises = exerciseService.query();		
+	//add exercises
+	$scope.addMuscleField = function(){
+		$scope.newExercise.muscles_worked.push("");
+	};	
+
+	$scope.addForceField = function(){
+		$scope.newExercise.force.push("");
+	};	
+
+	$scope.addEquipmentField = function(){
+		$scope.newExercise.equipment.push("");
+	};	
+	
+	$scope.addExercise = function(){
 		exerciseService.save($scope.newExercise, function(){
 			$location.path('/');
 		});
+	};
+
+	$scope.selectedExercise = {};
+	$scope.expand = function(exercise){
+		//BUG: makes the call during opening and closing
+		console.log($scope.selectedExercise);	
+		$scope.selectedExercise = exerciseService.get({id:exercise._id});	
 	};
 });
--- public/javascripts/controllers/routineController.js
+++ public/javascripts/controllers/routineController.js
@@ -3,9 +3,15 @@ var app = angular.module('workoutApp');
 app.controller('routineController', function($location, $http, $scope, routineService, $rootScope){
 	$scope.newRoutine = {title:''};
 	$scope.routines = routineService.query();
+	$scope.selectedRoutine = {};
+	$scope.isExpanded = false;
 	$scope.post = function(){
 		routineService.save($scope.newRoutine, function(){
 			$location.path('/');
 		});
+	};
+	$scope.expand = function(routine){
+		//BUG: makes the call during opening and closing
+		$scope.selectedRoutine = routineService.get({id:routine._id});	
 	};
 });
